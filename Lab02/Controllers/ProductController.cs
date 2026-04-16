c0d3e45b10fe733c8deffb4c87a7e2577cc0fd28
thy1411 <trannguyentienthy1411@gmail.com>
2026-04-16 16:40:24 +0700

    .

--- Lab02/Controllers/ProductController.cs
+++ Lab02/Controllers/ProductController.cs
@@ -7,15 +7,19 @@ namespace Lab02.Controllers
     {
         private static List<Product> dsSanPham = new List<Product>
         {
+            new Product {Id = 1, Name = "ThyThy", Price = 30000000}
         };
+        //trả về giao diện hiển thị  danh sách sản phẩm
         public IActionResult Index()
         {
             return View(dsSanPham);
         }
+        //trả về giao diện nhập thông tin sản phẩm cần thêm
         public IActionResult Create()
         {
             return View();
         }
+        //Xử lý thêm mới sản phẩm vào danh sách
         [HttpPost]
 
         public IActionResult Create(Product p)
